8180ba43946cd8ee4b74a2e2f1466b29c0bec48c
Shibly <shibly2016@gmail.com>
2020-05-30 16:17:49 +0530

    Updates

---
+++ precache-manifest.1bd898e8430fda037633f4663ffefcbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e0614f6abb7fa776ec2a9e3536cb740",
+    "url": "/perviewer/index.html"
+  },
+  {
+    "revision": "fc1ce374077dd8983fe0",
+    "url": "/perviewer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9b2f3bc14cd4446a9e6f",
+    "url": "/perviewer/static/css/main.a9ad41c5.chunk.css"
+  },
+  {
+    "revision": "fc1ce374077dd8983fe0",
+    "url": "/perviewer/static/js/2.fd0f5820.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/perviewer/static/js/2.fd0f5820.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2f3bc14cd4446a9e6f",
+    "url": "/perviewer/static/js/main.58a55bf3.chunk.js"
+  },
+  {
+    "revision": "597bec443d44a4768cc6",
+    "url": "/perviewer/static/js/runtime-main.b4764600.js"
+  }
+]);
